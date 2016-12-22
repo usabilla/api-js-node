1e6f29848667ac37d68292e6b7d1515581f0df3c
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-12-22 09:41:47 +0100

    Working wallaby with babel browserify

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,11 +1,19 @@
+const wallabify = require('wallabify');
+
 module.exports = function(wallaby) {
   return {
-    files: ['src/**/*.js'],
-    tests: ['test/**/*spec.js'],
-    testFramework: 'jasmine',
-    env: {
-      type: 'node',
-      runner: 'node'
+    files: [
+      {pattern: 'node_modules/babel-polyfill/dist/polyfill.js', instrument: false},
+      {pattern: 'src/**/*.js', load: false}
+    ],
+    tests: [{pattern: 'test/**/*spec.js', load: false}],
+    compilers: {
+      '**/*.js': wallaby.compilers.babel()
+    },
+    postprocessor: wallabify(),
+    setup: function() {
+      // required to trigger tests loading
+      window.__moduleBundler.loadTests();
     }
   };
 };
