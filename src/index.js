566d43333732b61adf4c70b4978ee3bdac8812ba
Sjoerd Huisman <shuisman@avivweb.com>
2016-12-30 11:28:13 +0100

    add strict everywhere

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 const SignatureFactory = require('./../src/signing.js');
 const resources = require('./../src/resources.js');
 
--- src/signing.js
+++ src/signing.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 const moment = require('moment');
 const crypto = require('crypto');
 
