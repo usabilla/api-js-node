857b90c66a7215ae20ca25d908aa6431b4783264
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2017-01-02 09:28:04 +0100

    Removed boilerplate code

--- test/resources.spec.js
+++ test/resources.spec.js
@@ -5,29 +5,17 @@ describe('Resources', function() {
     beforeEach(function() {
       this.websitesProduct = new resources.WebsitesProduct();
     });
-
-    it('new', function() {
-      expect(true).toEqual(true);
-    });
   });
 
   describe('EmailProduct', function() {
     beforeEach(function() {
       this.emailProduct = new resources.EmailProduct();
     });
-
-    it('new', function() {
-      expect(true).toEqual(true);
-    });
   });
 
   describe('AppsProduct', function() {
     beforeEach(function() {
       this.appsProduct = new resources.AppsProduct();
-    });
-
-    it('new', function() {
-      expect(true).toEqual(true);
     });
   });
 });
