5eb82611ca5f4f90fc3740412b8d9f4c4e50cc26
Sjoerd Huisman <shuisman@avivweb.com>
2017-12-18 09:50:45 +0100

    refactor(campaigns): reduce complexity by removing inheritance and duplicating code to enhance readability and uniformity

--- src/resources/appsCampaignsResource.js
+++ src/resources/appsCampaignsResource.js
@@ -1,11 +1,19 @@
-const CampaignsResource = require('./campaignsResource');
+const CampaignsResultsResource = require('./campaignsResultsResource');
+const Resource = require('./resource');
 
 /**
  * Apps Campaigns resource.
  */
-class AppsCampaignsResource extends CampaignsResource {
+class AppsCampaignsResource extends Resource {
   constructor(base, signatureFactory, config) {
-    super(base, signatureFactory, config);
+    const baseUrl = `${base}/campaign`;
+    super(baseUrl, signatureFactory, config);
+
+    this.results = new CampaignsResultsResource(
+      baseUrl,
+      signatureFactory,
+      config
+    );
   }
 }
 
--- src/resources/webCampaignsResource.js
+++ src/resources/webCampaignsResource.js
@@ -1,14 +1,21 @@
-const CampaignsResource = require('./campaignsResource');
+const CampaignsResultsResource = require('./campaignsResultsResource');
 const WebCampaignsStatsResource = require('./webCampaignsStatsResource');
+const Resource = require('./resource');
 
 /**
  * Websites Campaigns resource.
  */
-class WebCampaignsResource extends CampaignsResource {
+class WebCampaignsResource extends Resource {
   constructor(base, signatureFactory, config) {
-    super(base, signatureFactory, config);
+    const baseUrl = `${base}/campaign`;
+    super(baseUrl, signatureFactory, config);
 
-    this.stats = new WebCampaignsStatsResource(this.baseUrl, signatureFactory, config);
+    this.results = new CampaignsResultsResource(
+      baseUrl,
+      signatureFactory,
+      config
+    );
+    this.stats = new WebCampaignsStatsResource(baseUrl, signatureFactory, config);
   }
 }
 
